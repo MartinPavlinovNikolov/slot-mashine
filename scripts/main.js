e80943b0e9feca1a2ff166b100076cef1f3250bb
Martin <martin_nikolov.89@abv.bg>
2018-07-10 01:44:26 +0300

    controls done;start working on wining logic

--- scripts/main.js
+++ scripts/main.js
@@ -48,51 +48,10 @@
 
 				substract(value);
 
-				let placeForDot;
-				switch (_value.toString().length){
-					case 7:
-						placeForDot = 5;
-						break;
-					case 6:
-						placeForDot = 4;
-						break;
-					case 5:
-						placeForDot = 3;
-						break;
-					case 4:
-						placeForDot = 2;
-						break;
-					case 3:
-						placeForDot = 1;
-						break;
-					case 2:
-						placeForDot = 0;
-						break;
-					case 1:
-						placeForDot = null;
-						break;
-				}
+				let newValue = (_value / 100).toFixed(2).toString();
 
-				let newValue;
-				if(placeForDot === null){
-					newValue = _value.toString();
-				}else{
-					newValue = [_value.toString().slice(0, placeForDot), '.', _value.toString().slice(placeForDot)].join('');
-				}
-
-				switch (newValue.length){
-					case 6:
-						newValue = newValue + '0';
-						break;
-					case 4:
-						newValue = '0' + newValue;
-						break;
-					case 3:
-						newValue = '00' + newValue;
-						break;
-					case 1:
-						newValue = '00.0' + newValue;
-						break;
+				if(newValue.substr(1, 1) == '.'){
+					newValue = '0' + newValue;
 				}
 
 				$(_element).text(newValue);
@@ -248,6 +207,14 @@
 			'image_moving_point': '400'
 		};
 
+		const grid = {
+			'rows': {
+				'first': [],
+				'second': [],
+				'third': []
+			}
+		};
+
 		function spin(col = 1){
 			
 			setTimeout(function(){
@@ -267,7 +234,12 @@
 					Animator.spin(col);
 				}else{
 					setTimeout(function(){
-						$('#spin').removeClass('disabled').addClass('active');
+						if(Amount.getAmount() < 5){
+							$('#spin').removeClass('active').addClass('disabled');
+						}else{
+							$('#spin').removeClass('disabled').addClass('active');
+						}
+
 					}, 3500);
 				}
 			}, 200);
@@ -291,6 +263,27 @@
 						that.element.append("<img src='images/" + Math.ceil(Math.random()*Math.ceil(6)) + ".png'></img>");
 					}
 					iterations--;
+
+					if(iterations === 0){
+						let img_number = that.element.children().first().prop('src').substr(-5, 1);
+						if(that.position.top > 266 && that.position.top < 400){
+							grid.rows.third.push(img_number);
+						}else if(that.position.top > 133 && that.position.top < 200){
+							grid.rows.second.push(img_number);
+						}else if(that.position.top > -1 && that.position.top < 120){
+							grid.rows.first.push(img_number);
+						}
+
+						if(that.element.prop('id') === 'row-1-col-5'){
+
+							Animator.anyWining(grid);
+
+							grid.rows.first = [];
+							grid.rows.second = [];
+							grid.rows.third = [];
+						}
+					}
+
 					that.startSpining(iterations);
 				});
 			}
@@ -299,6 +292,501 @@
 		function setPositions(top, left){
 			this.position.top = top;
 			this.position.left = left;
+		}
+
+		function anyWining(arg){
+			const _1_1 = arg.rows.first[0];
+			const _1_2 = arg.rows.first[1];
+			const _1_3 = arg.rows.first[2];
+			const _1_4 = arg.rows.first[3];
+			const _1_5 = arg.rows.first[4];
+			const _2_1 = arg.rows.second[0];
+			const _2_2 = arg.rows.second[1];
+			const _2_3 = arg.rows.second[2];
+			const _2_4 = arg.rows.second[3];
+			const _2_5 = arg.rows.second[4];
+			const _3_1 = arg.rows.third[0];
+			const _3_2 = arg.rows.third[1];
+			const _3_3 = arg.rows.third[2];
+			const _3_4 = arg.rows.third[3];
+			const _3_5 = arg.rows.third[4];
+
+			let points = 0;
+
+			// xx---
+			// -----
+			// -----
+			if(_1_1 === _1_2){
+
+				// xxx--
+				// -----
+				// -----
+				if(_1_1 === _1_3){
+
+					// xxxx-
+					// -----
+					// -----
+					if(_1_1 === _1_4){
+						points += 2;
+					}
+
+					// xxx--
+					// ---x-
+					// -----
+					if(_1_1 === _2_4){
+						points += 2;
+					}
+
+					points++;
+				}
+
+				// xx---
+				// --x--
+				// -----
+				if(_1_1 === _2_3){
+					
+					// xx-x-
+					// --x--
+					// -----
+					if(_1_1 === _1_4){
+						points += 2;
+					}
+
+					// xx---
+					// --xx-
+					// -----
+					if(_1_1 === _2_4){
+						points += 2;
+					}
+
+					// xx---
+					// --x--
+					// ---x-
+					if(_1_1 === _3_4){
+						points += 2;
+					}
+
+					points++;
+				}
+
+			}
+
+			// x----
+			// -x---
+			// -----
+			if(_1_1 === _2_2){
+
+				// x-x--
+				// -x---
+				// -----
+				if(_1_1 === _1_3){
+					
+					// x-xx-
+					// -x---
+					// -----
+					if(_1_1 === _1_4){
+						points += 2;
+					}
+
+					// x-x--
+					// -x-x-
+					// -----
+					if(_1_1 === _2_4){
+						points += 2;
+					}
+
+					points++;
+				}
+
+				// x----
+				// -xx--
+				// -----
+				if(_1_1 === _2_3){
+
+					// x--x-
+					// -xx--
+					// -----
+					if(_1_1 === _1_4){
+						points += 2;
+					}
+
+					// x----
+					// -xxx-
+					// -----
+					if(_1_1 === _2_4){
+						points += 2;
+					}
+
+					// x----
+					// -xx--
+					// ---x-
+					if(_1_1 === _3_4){
+						points += 2;
+					}
+
+					points++;
+				}
+
+				// x----
+				// -x---
+				// --x--
+				if(_1_1 === _3_3){
+					
+					// x----
+					// -x-x-
+					// --x--
+					if(_1_1 === _2_4){
+						points += 2;
+					}
+
+					// x----
+					// -x---
+					// --xx-
+					if(_1_1 === _3_4){
+						points += 2;
+					}
+
+					points++;
+				}
+
+			}
+			
+			// -x---
+			// x----
+			// -----
+			if(_2_1 === _1_2){
+
+				// -xx--
+				// x----
+				// -----
+				if(_2_1 === _1_3){
+
+					// -xxx-
+					// x----
+					// -----
+					if(_2_1 === _1_4){
+						points += 2;
+					}
+
+					// -xx--
+					// x--x-
+					// -----
+					if(_2_1 === _2_4){
+						points += 2;
+					}
+
+					points++;
+				}
+
+				// -x---
+				// x-x--
+				// -----
+				if(_2_1 === _2_3){
+
+					// -x-x-
+					// x-x--
+					// -----
+					if(_2_1 === _1_4){
+						points += 2;
+					}
+
+					// -x---
+					// x-xx-
+					// -----
+					if(_2_1 === _2_4){
+						points += 2;
+					}
+
+					// -x---
+					// x-x--
+					// ---x-
+					if(_2_1 === _3_4){
+						points += 2;
+					}
+
+					points++;
+				}
+
+			}
+			
+			// -----
+			// xx---
+			// -----
+			if(_2_1 === _2_2){
+
+				// --x--
+				// xx---
+				// -----
+				if(_2_1 === _1_3){
+
+					// --xx-
+					// xx---
+					// -----
+					if(_2_1 === _1_4){
+						points += 2;
+					}
+
+					// --x--
+					// xx-x-
+					// -----
+					if(_2_1 === _2_4){
+						points += 2;
+					}
+
+					points++;
+				}
+
+				// -----
+				// xxx--
+				// -----
+				if(_2_1 === _2_3){
+
+					// ---x-
+					// xxx--
+					// -----
+					if(_2_1 === _1_4){
+						points += 2;
+					}
+
+					// -----
+					// xxxx-
+					// -----
+					if(_2_1 === _2_4){
+						points += 2;
+					}
+
+					// -----
+					// xxx--
+					// ---x-
+					if(_2_1 === _3_4){
+						points += 2;
+					}
+
+					points++;
+				}
+
+				// -----
+				// xx---
+				// --x--
+				if(_2_1 === _3_3){
+
+					// -----
+					// xx-x-
+					// --x--
+					if(_2_1 === _2_4){
+						points += 2;
+					}
+
+					// -----
+					// xx---
+					// --xx-
+					if(_2_1 === _3_4){
+						points += 2;
+					}
+
+					points++;
+				}
+
+			}
+			
+			// -----
+			// x----
+			// -x---
+			if(_2_1 === _3_2){
+
+				// -----
+				// x-x--
+				// -x---
+				if(_2_1 === _2_3){
+
+					// ---x-
+					// x-x--
+					// -x---
+					if(_2_1 === _1_4){
+						points += 2;
+					}
+
+					// -----
+					// x-xx-
+					// -x---
+					if(_2_1 === _2_4){
+						points += 2;
+					}
+
+					// -----
+					// x-x--
+					// -x-x-
+					if(_2_1 === _3_4){
+						points += 2;
+					}
+
+					points++;
+				}
+
+				// -----
+				// x----
+				// -xx--
+				if(_2_1 === _3_3){
+
+					// -----
+					// x--x-
+					// -xx--
+					if(_2_1 === _2_4){
+						points += 2;
+					}
+
+					// -----
+					// x----
+					// -xxx-
+					if(_2_1 === _3_4){
+						points += 2;
+					}
+
+					points++;
+				}
+
+			}
+			
+			// -----
+			// -x---
+			// x----
+			if(_3_1 === _2_2){
+
+				// --x--
+				// -x---
+				// x----
+				if(_3_1 === _1_3){
+
+					// --xx-
+					// -x---
+					// x----
+					if(_3_1 === _1_4){
+						points += 2;
+					}
+
+					// --x--
+					// -x-x-
+					// x----
+					if(_3_1 === _2_4){
+						points += 2;
+					}
+
+					points++;
+				}
+
+				// -----
+				// -xx--
+				// x----
+				if(_3_1 === _2_3){
+
+					// ---x-
+					// -xx--
+					// x----
+					if(_3_1 === _1_4){
+						points += 2;
+					}
+
+					// -----
+					// -xxx-
+					// x----
+					if(_3_1 === _2_4){
+						points += 2;
+					}
+
+					// -----
+					// -xx--
+					// x--x-
+					if(_3_1 === _3_4){
+						points += 2;
+					}
+
+					points++;
+				}
+
+				// -----
+				// -x---
+				// x-x--
+				if(_3_1 === _3_3){
+
+					// -----
+					// -x-x-
+					// x-x--
+					if(_3_1 === _2_4){
+						points += 2;
+					}
+
+					// -----
+					// -x---
+					// x-xx-
+					if(_3_1 === _3_4){
+						points += 2;
+					}
+
+					points++;
+				}
+
+			}
+			
+			// -----
+			// -----
+			// xx---
+			if(_3_1 === _3_2){
+
+				// -----
+				// --x--
+				// xx---
+				if(_3_1 === _2_3){
+
+					// ---x-
+					// --x--
+					// xx---
+					if(_3_1 === _1_4){
+						points += 2;
+					}
+
+					// -----
+					// --xx-
+					// xx---
+					if(_3_1 === _2_4){
+						points += 2;
+					}
+
+					// -----
+					// --x--
+					// xx-x-
+					if(_3_1 === _3_4){
+						points += 2;
+					}
+
+					points++;
+				}
+
+				// -----
+				// -----
+				// xxx--
+				if(_3_1 === _3_3){
+
+					// -----
+					// ---x-
+					// xxx--
+					if(_3_1 === _2_4){
+						points += 2;
+					}
+
+					// -----
+					// -----
+					// xxxx-
+					if(_3_1 === _3_4){
+						points += 2;
+					}
+
+					points++;
+				}
+
+			}
+
+			console.log(points * Bet.getBet());
+
 		}
 
 		const Rows = (function(){
@@ -329,7 +817,8 @@
 		return {
 			config,
 			spin,
-			Rows
+			Rows,
+			anyWining
 		};
 	})();
 
@@ -340,10 +829,10 @@
 		
 		e.preventDefault();
 
-		Amount.changeElementValue(Bet.getBet());
-
-		if($(this).hasClass('active')){
+		if(Amount.getAmount() >= 5){
 			
+			Amount.changeElementValue(Bet.getBet());
+
 			$(this).removeClass('active').addClass('disabled');
 
 			Animator.spin();
